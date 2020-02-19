131024c3d029a82f5ffc6307c477258dcb9dd252
taicheng <iamdennisme@gmail.com>
2020-02-20 01:27:51 +0800

    first commit

---
+++ app/src/main/java/com/dennisce/pluginstudydemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dennisce.pluginstudydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
